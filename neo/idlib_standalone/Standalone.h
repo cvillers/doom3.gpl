c8d3df031244fff033549b789009f6b585b61b4c
Cameron Villers <cameron@ville.rs>
2011-11-26 14:54:35 -0500

    Updated Standalone.h to better describe behavior

--- neo/idlib_standalone/Standalone.h
+++ neo/idlib_standalone/Standalone.h
@@ -17,8 +17,11 @@ public:
 	// Initializes the standalone idLib. basePath is equivalent to
 	// the fs_basepath cvar (/home/user/doom, c:\doom, etc).
 	//
-	// If any of these are not set then basePath will be set from
-	// the environment variable IDLIB_BASE_PATH and everything will
+	// If any of these are not set then the paths will be set from
+	// the environment variables IDLIB_BASEPATH, IDLIB_SAVEPATH,
+	// IDLIB_CDPATH, and IDLIB_DEVPATH. If those are not set then
+	// the filesystem will fall back to its standard methods of
+	// finding those paths.
 	// be referenced to there. That is effected by implementing the
 	// Sys_* path functions.
 	static void Init(const char *basePath = "basePath", const char *savePath = "savePath", const char *cdPath = "cdPath", const char *devPath = "devPath");
